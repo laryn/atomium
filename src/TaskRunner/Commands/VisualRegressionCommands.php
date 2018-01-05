207495f695ee65d1d3f2d9bbf1c405d0e721d611
Antonio De Marco <antonio@nuvole.org>
2018-01-05 14:15:03 +0100

    Issue #62: Initial work.

---
+++ src/TaskRunner/Commands/VisualRegressionCommands.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Drupal\atomium\TaskRunner\Commands;
+
+use EC\OpenEuropa\TaskRunner\Commands\BaseCommands;
+
+/**
+ * Class VisualRegressionCommands.
+ *
+ * @package My\Custom\TaskRunner\Commands
+ */
+class VisualRegressionCommands extends BaseCommands {
+
+  /**
+   * Test command.
+   *
+   * @command atomium:command-one
+   */
+  public function commandOne() {
+  }
+
+}
